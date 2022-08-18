463f65146d047202177d58ad3a9f9eaf38727a32
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-08-18 13:59:00 +0500

    qo'shimcha o'zgartirish

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,11 @@
 import os
 from pathlib import Path
+from environs import Env
+
+# Environment variables
+env = Env()
+env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,10 +15,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(z%kmf5&67gd1cay!$vymngh1h#%+3w+0(ytjt+k3-&han!pd6'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1','localhost']
 
